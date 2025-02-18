b6d187b3a53cdf2ff31c2881ef59a974fe41fc57
Mjames951 <aspc.james2@gmail.com>
2025-02-17 19:00:25 -0600

    chat forms and things

--- chat/chatforms.py
+++ chat/chatforms.py
@@ -11,7 +11,35 @@ class ChannelSectionForm(forms.ModelForm):
         model = ChannelSection
         fields = ['name']
 
+    def is_valid(self):
+        valid = super(ChannelSectionForm, self).is_valid()
+        
+        invalidChars = [ '/' ]
+
+        try: 
+            if any([c in self.cleaned_data.get('name') for c in invalidChars]):
+                self.add_error('name', 'no slashes in your name, it messes with the url patterns')
+                valid = False
+        except:
+            valid = False
+
+        return valid
+
 class ChannelForm(forms.ModelForm):
     class Meta:
         model = Channel
         fields = ['section', 'name']
+
+    def is_valid(self):
+        valid = super(ChannelForm, self).is_valid()
+        
+        invalidChars = [ '/' ]
+
+        try: 
+            if any([c in self.cleaned_data.get('name') for c in invalidChars]):
+                self.add_error('name', 'no slashes in your name, it messes with the url patterns')
+                valid = False
+        except:
+            valid = False
+
+        return valid
