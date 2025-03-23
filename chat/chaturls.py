142d968bd9b8e078e7d715eb35e16bad973c22c6
SpicyChair <aspc.james2@gmail.com>
2025-03-23 14:22:11 -0500

    start of report implementation

--- chat/chaturls.py
+++ chat/chaturls.py
@@ -3,5 +3,7 @@ from . import chatviews as views
 
 urlpatterns = [
     path('', views.chat, name='chat'),
-    path('<channelname>/<int:load>/', views.channel, name="channel")
+    path('<channelname>/<int:load>/', views.channel, name="channel"),
+    path('report/', views.nothin, name="chatreport"),
+    path('report/<postid>/', None)
 ]
