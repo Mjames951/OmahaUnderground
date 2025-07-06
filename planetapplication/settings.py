5eba0a112485db7c3e8e5ad60e1a9c065875f639
SpicyChair <aspc.james2@gmail.com>
2025-07-05 20:46:48 -0500

    settings reorder and redundancy removal

--- planetapplication/settings.py
+++ planetapplication/settings.py
@@ -1,7 +1,5 @@
 from pathlib import Path
-import os
 import environ
-from storages.backends.s3boto3 import S3Boto3Storage
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env()
@@ -14,27 +12,74 @@ BAND_WIDTH_HEIGHT = 500 #width and height of band/label profile pictures
 SHOW_MAX_WIDTH_HEIGHT = 800 #max width and/or height of show posters
 CHAT_LOAD = 20 #how many chat messages are loaded at a time
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(BASE_DIR / '.env')
-
+environ.Env.read_env(BASE_DIR / '.env', overwrite=True)
 DEBUG = env.bool('DEBUG')
+
 
 # HTPPS stuff
 SESSION_COOKIE_SECURE = not DEBUG
 CSRF_COOKIE_SECURE = not DEBUG
 
+
+#important!! 
 SECRET_KEY = env.str('SECRET_KEY', default=get_random_secret_key())
 DATABASE_URL = env.str('DATABASE_URL')
 
+
+#hosts
 ALLOWED_HOSTS = ["omahaunderground-wispy-meadow-6277.fly.dev", 'localhost', 'omahaunderground.net']
 if DEBUG:
     ALLOWED_HOSTS.append('127.0.0.1')
 
 CSRF_TRUSTED_ORIGINS = ['https://omahaunderground-wispy-meadow-6277.fly.dev', 'https://omahaunderground.net']
 
-# Application definition
+
+# Database
+if DEBUG and (not DATABASE_URL or DATABASE_URL == 'None'): #if debug=True but no database URL is provided (development)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    print("using this")
+    DATABASES = {
+        'default': env.db(),
+    }
+
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_ENDPOINT_URL_S3 = env('AWS_ENDPOINT_URL_S3')
+AWS_ENDPOINT_URL_IAM = env('AWS_ENDPOINT_URL_IAM')
+AWS_REGION = env('AWS_REGION')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_LOCATION = 'static/'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_FILE_OVERWRITE = False
+MEDIA_URL = 'https://omaha-underground.t3.storage.dev/static/'
+
+
+#if not in development (production), use s3 static storage
+if not DEBUG:
+    STATIC_URL = 'https://omaha-underground.t3.storage.dev/static/'
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3.S3Storage",
+        }
+    }
+else:
+    STATIC_URL = '/static/'
+
+
 
 INSTALLED_APPS = [
     'django.contrib.sessions',
@@ -67,8 +112,35 @@ MIDDLEWARE = [
     'tz_detect.middleware.TimezoneMiddleware',
 ]
 
+
+
+WSGI_APPLICATION = 'planetapplication.wsgi.application'
 ROOT_URLCONF = 'planetapplication.urls'
 
+#Custom User Logic
+LOGIN_REDIRECT_URL = '/'
+AUTH_USER_MODEL = "users.CustomUser"
+AUTHENTICATION_BACKENDS = ['users.authentication.BackendAuth']
+
+#EMAIL SHTUFF
+ADMIN_EMAIL = env('ADMIN_EMAIL')
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL')
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#template processing
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -85,26 +157,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'planetapplication.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-if DEBUG and not DATABASE_URL:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-        'default': env.db(),
-    }
-
 
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -119,72 +173,3 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGIN_REDIRECT_URL = '/'
-
-AUTH_USER_MODEL = "users.CustomUser"
-AUTHENTICATION_BACKENDS = ['users.authentication.BackendAuth']
-
-
-#EMAIL SHTUFF
-ADMIN_EMAIL = env('ADMIN_EMAIL')
-EMAIL_BACKEND = env('EMAIL_BACKEND')
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS')
-EMAIL_USE_SSL = env('EMAIL_USE_SSL')
-
-
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
-AWS_ENDPOINT_URL_S3 = env('AWS_ENDPOINT_URL_S3')
-AWS_ENDPOINT_URL_IAM = env('AWS_ENDPOINT_URL_IAM')
-AWS_REGION = env('AWS_REGION')
-AWS_DEFAULT_ACL = 'public-read'
-AWS_LOCATION = 'static/'
-AWS_QUERYSTRING_AUTH = False
-
-
-class PublicMediaStorage(S3Boto3Storage):
-    location = 'media'
-    default_acl = 'public-read'
-    file_overwrite = False
-
-MEDIA_URL = 'https://omaha-underground.t3.storage.dev/static/'
-
-#if not in development (production), use s3 static storage
-if not DEBUG:
-    STATIC_URL = 'https://omaha-underground.t3.storage.dev/static/'
-    STORAGES = {
-        "default": {
-            "BACKEND": "storages.backends.s3.S3Storage",
-            "OPTIONS": {
-
-            },
-        },
-        "staticfiles": {
-            "BACKEND": "storages.backends.s3.S3Storage",
-            "OPTIONS": {
-
-            },
-        }
-    }
-else:
-    STATIC_URL = '/static/'
