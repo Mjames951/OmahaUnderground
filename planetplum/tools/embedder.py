967a28f9da8e1151d4dbacca3c58cbb79cfb2e54
Matthew Fuentes James <118388822+Mjames951@users.noreply.github.com>
2026-08-12 18:48:24 +0000

    FIXES #34

--- planetplum/tools/embedder.py
+++ planetplum/tools/embedder.py
@@ -10,7 +10,7 @@ def bandcamp(url):
 
             escaped_src = video.video
 
-            html = f'''<iframe src="{escaped_src}" style="margin: .5rem auto; text-align: center; display:block;" width="{video['video:width']}" height="{video['video:height']}" scrolling="no" frameborder="0" allowfullscreen></iframe>'''
+            html = f'''<iframe src="{escaped_src}" style="margin: .5rem auto; text-align: center; display:block; width:100%; max-width: {video['video:width']}px;" height="{video['video:height']}" scrolling="no" frameborder="0" allowfullscreen></iframe>'''
             return html
         except:
             return ""
